2d645c850b8de12c1577da91ccc3733fba659ea0
shuvakarmakar <shuva15-7953@diu.edu.bd>
2024-12-10 18:15:07 +0600

    Tailwind Cofig Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
+  themes: {
+    extends: {}
   },
-  plugins: [
-    require('daisyui'),
-  ],
-  themes: ["light"],
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["light"],
+  }
 }
